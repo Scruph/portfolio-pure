e8f7bfde636220c306a65618e6ab125a2fc63067
Jaine Scruph <jscruph10@gmail.com>
2021-12-01 16:27:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// const linksSocialMedia = {
+//   github: 'Scruph',
+//   youtube: 'RocketSeat',
+//   facebook: 'jscruph',
+//   instagram: 'j_scruph',
+//   twitter: 'j_scruph'
+// }
+
+// function changeSocialMediaLinks(){
+//   for (let li of socialLinks.children){
+//     const social = li.getAttribute('class')
+//     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
+//   }
+// }
+
+// changeSocialMediaLinks()
+
+// function getGitHubProfileInfos(){
+//   const url = `https://api.github.com/users/${linksSocialMedia.github}`
+//   fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//      userName.textContent = data.name
+//      userBio.textContent = data.bio
+//      userLink.href = data.html_url
+//      UserImage.src = data.avatar_url
+//      userLogin.textContent = data.login
+//     })
+// }
+// getGitHubProfileInfos()
